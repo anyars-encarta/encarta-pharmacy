5e84d4289e82cf9fd00e513a4455ad7bbded8e4c
anyars-encarta <anyarsencarta@gmail.com>
2024-02-03 11:42:01 +0000

    Initial rails and react setup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import User from "./User";
+
+const App = () => {
+    return (
+      <User />
+    )
+}
+
+export default App;
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    validates :full_name, presence: true
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
+end
