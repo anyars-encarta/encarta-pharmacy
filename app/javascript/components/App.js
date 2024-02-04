7cd5bba1eb43813db7cfa1f3c8bba72d7f1f4717
anyars-encarta <anyarsencarta@gmail.com>
2024-02-04 20:24:25 +0000

    Create products slices and store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 // import SuppliersList from './SuppliersList';
-import UsersList from './UsersList';
+// import UsersList from './UsersList';
 // import CategoriesList from './CategoriesList';
+import ProductsList from './ProductsList';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
-        <UsersList />
-        {/* <CategoriesList /> */}
         {/* <SuppliersList /> */}
+        {/* <UsersList /> */}
+        {/* <CategoriesList /> */}
+        <ProductsList />
       </div>
     </Provider>
   );
--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import suppliersReducer from './suppliers/suppliersSlice';
 import usersReducer from './users/usersSlice';
 import categoriesReducer from './categories/categoriesSlice';
+import productsReducer from './products/productsSlice';
 
 const store = configureStore({
   reducer: {
     suppliers: suppliersReducer,
     users: usersReducer,
     categories: categoriesReducer,
+    products: productsReducer,
   },
 });
 
