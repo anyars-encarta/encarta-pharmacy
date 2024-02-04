68e8a62f30ea90f66a59115262321702f2ea47e2
anyars-encarta <anyarsencarta@gmail.com>
2024-02-04 19:50:13 +0000

    Correct relationships in models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   validates :category_name, uniqueness: true, presence: true
+  
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+    validates :product_name, uniqueness: true, presence: true
+    validates :category_id, presence: true
+
+    belongs_to :category
 end
